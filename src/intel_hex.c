9e31d30efcaa17d8789ad2f0b4910ee53fb4e179
schmidtw <schmidtw@83de3fdd-5b20-0410-aa69-c1ebc1cb175e>
2005-09-25 01:27:42 +0000

    * src/{*.c,*.h}, AUTHORS: removed the copyright information from the file so that the AUTHORS file can manage this information for the entire project

--- src/intel_hex.c
+++ src/intel_hex.c
@@ -15,8 +15,6 @@
  *
  * $Id$
  *
- * Copyright (C) 2005 Weston Schmidt <weston_schmidt@yahoo.com>
- * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
