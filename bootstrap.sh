3812b00d53bf6695f6ad1e650895250ec83e310a
Cameron Tacklind <cameron@tacklind.com>
2022-10-20 15:17:04 -0700

    Remove generated files

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,10 @@
 set -e # exit on error
 set -x # Enable echoing of commands
 
+mkdir -p m4
 aclocal -I m4
 autoheader
-automake --foreign --add-missing --copy
+automake --foreign --add-missing --force-missing
 autoconf
 
 set +x # Disable echoing of commands
