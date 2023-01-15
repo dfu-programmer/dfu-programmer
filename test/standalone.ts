52f1479ec21a2642d62c64d6d637189e32f00a58
Cameron Tacklind <cameron@tacklind.com>
2023-01-14 23:51:27 -0800

    Check usage functionality

--- test/standalone.ts
+++ test/standalone.ts
@@ -9,7 +9,38 @@ describe("standalone", () => {
   test("it runs without arguments and returns an error code and usage", async () => {
     const res = runDfu();
     expect(await res.exitCode).toBe(2);
-    expect(res.stdout).toBe("");
-    expect(res.stderr).toMatch(/^dfu-programmer/g);
+    const { stdout, stderr } = res;
+
+    // No output on stdout
+    expect(stdout).toBe("");
+
+    // Starts with "dfu-programmer <version>"
+    expect(stderr).toMatch(/^dfu-programmer/g);
+  });
+
+  test("it prints usage with --help", async () => {
+    const res = runDfu(["--help"]);
+    expect(await res.exitCode).toBe(0);
+    const { stdout, stderr } = res;
+
+    // No output on stdout
+    expect(stdout).toBe("");
+
+    // Starts with "dfu-programmer <version>"
+    expect(stderr).toMatch(/^dfu-programmer/g);
+
+    // Contains the usage
+    expect(stderr).toMatch(/^Usage: dfu-programmer target[:usb-bus,usb-addr] command [options] [global-options] [file|data]/m);
+
+    // Contains the global options
+    expect(stderr).toMatch(/^global-options:/m);
+
+    // Contains the command summary
+    expect(stderr).toMatch(/^command summary:/m);
+
+    // Contains the additional details
+    expect(stderr).toMatch(/^additional details:/m);
+
+    // ...
   });
 });
